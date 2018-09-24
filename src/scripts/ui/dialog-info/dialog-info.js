c68752151628c142b7ebc51d8a19939952a9f9b5
Joel Launder <jalaunder@gmail.com>
2018-09-24 17:31:24 +1000

    Offline download fix
    No longer display download for offline if device does not support it

--- src/scripts/ui/dialog-info/dialog-info.js
+++ src/scripts/ui/dialog-info/dialog-info.js
@@ -47,51 +47,53 @@ function openInfoDialog() {
             "<br>" +
             "</p>").appendTo(infoDialogText);
 
-        $("<h2 class='subheading'>Use Offline</h2>").appendTo(infoDialogText);
-        $("<p>" +
-            "Download the card images to your device so that you can use Kingdom even when offline. " +
-            "The download is about 70MB so it is recommended you complete the download while connected to a Wi-Fi network." +
-            "</p>").appendTo(infoDialogText);
-        var cacheCards = $("<div class='btn cache-cards'>Download Cards</div>").appendTo(infoDialogInner);
-        cacheCards.on('click', function () {
-            if (!isDownloadingCards) {
-                isDownloadingCards = true;
-                caches.open(CACHE_NAME).then(function (cache) {
-                    // Read resources
-                    var resourceIndex = 0;
-                    var loadedResourceIndex = 0;
-                    cacheCards.html(0 + "/" + kingdomResourcesCardImages.length);
-                    var checkCache = window.setInterval(function () {
-                        if (loadedResourceIndex >= resourceIndex) {
-                            if (resourceIndex < kingdomResourcesCardImages.length) {
-                                cache.add(kingdomResourcesCardImages[resourceIndex]);
-                                ++resourceIndex;
-                            }
-                        }
-
-                        if (loadedResourceIndex < resourceIndex) {
-                            caches.match(kingdomResourcesCardImages[loadedResourceIndex]).then(function (response) {
-                                if (response !== undefined && response.ok) {
-                                    if (loadedResourceIndex < resourceIndex) {
-                                        ++loadedResourceIndex;
-                                    }
+        if ('serviceWorker' in navigator) {
+            $("<h2 class='subheading'>Use Offline</h2>").appendTo(infoDialogText);
+            $("<p>" +
+                "Download the card images to your device so that you can use Kingdom even when offline. " +
+                "The download is about 70MB so it is recommended you complete the download while connected to a Wi-Fi network." +
+                "</p>").appendTo(infoDialogText);
+            var cacheCards = $("<div class='btn cache-cards'>Download Cards</div>").appendTo(infoDialogInner);
+            cacheCards.on('click', function () {
+                if (!isDownloadingCards) {
+                    isDownloadingCards = true;
+                    caches.open(CACHE_NAME).then(function (cache) {
+                        // Read resources
+                        var resourceIndex = 0;
+                        var loadedResourceIndex = 0;
+                        cacheCards.html(0 + "/" + kingdomResourcesCardImages.length);
+                        var checkCache = window.setInterval(function () {
+                            if (loadedResourceIndex >= resourceIndex) {
+                                if (resourceIndex < kingdomResourcesCardImages.length) {
+                                    cache.add(kingdomResourcesCardImages[resourceIndex]);
+                                    ++resourceIndex;
                                 }
-                            });
+                            }
 
-                            cacheCards.html(resourceIndex + "/" + kingdomResourcesCardImages.length);
-                        }
+                            if (loadedResourceIndex < resourceIndex) {
+                                caches.match(kingdomResourcesCardImages[loadedResourceIndex]).then(function (response) {
+                                    if (response !== undefined && response.ok) {
+                                        if (loadedResourceIndex < resourceIndex) {
+                                            ++loadedResourceIndex;
+                                        }
+                                    }
+                                });
 
-                        if (loadedResourceIndex >= kingdomResourcesCardImages.length) {
-                            cacheCards.html("Done");
-                            isDownloadingCards = false;
-                            window.clearInterval(checkCache);
-                        }
-                    }, 10);
-                });
-            } else {
-                cacheCards.html("Download in Progress");
-            }
-        });
+                                cacheCards.html(resourceIndex + "/" + kingdomResourcesCardImages.length);
+                            }
+
+                            if (loadedResourceIndex >= kingdomResourcesCardImages.length) {
+                                cacheCards.html("Done");
+                                isDownloadingCards = false;
+                                window.clearInterval(checkCache);
+                            }
+                        }, 10);
+                    });
+                } else {
+                    cacheCards.html("Download in Progress");
+                }
+            });
+        }
 
         var canAccessStoarge = false;
         if (typeof (Storage) !== undefined) {
